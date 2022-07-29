175001bf4a4de241b20743e47709f7b22f7bcbd1
Max <maxlovesairandteslas@gmail.com>
2022-07-28 21:52:18 -0700

    add files

---
+++ homework11/Greedy Gift Givers/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+    freopen("gift1.in", "r", stdin);
+    freopen("gift1.out", "w", stdout);
+    int np;
+    cin>>np;
+    int peoplecount[np];
+    for(int i = 0;i<np;i++) peoplecount[np] = 0;
+    string people[np];
+    for(int i = 0;i<np;i++){
+        cin>>people[np];
+    }
+
+}
