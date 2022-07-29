e136e7645716ea76399a69c601b533be4e35b932
Max <maxlovesairandteslas@gmail.com>
2022-07-28 20:01:54 -0700

    Update game.cpp

---
+++ homework11/game.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main() {
+
+  bool reached = false;
+  stack<int> s;
+  int nums, score, int1;
+  cin>>nums;    
+  char curr;
+  for(int i = 0;i<nums;i++){
+    cin>>curr;
+    if (curr<='9'&&curr>='0'){
+      int1 = curr - '0';
+    }else if(curr = '+'){
+      while((reached)){
+
+      }
+    }
+  }
+
+}
