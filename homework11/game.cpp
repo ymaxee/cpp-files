8a9b84d6086fa5085f97f88127bbed861fa483c1
Max <maxlovesairandteslas@gmail.com>
2022-07-28 21:05:01 -0700

    change

--- homework11/game.cpp
+++ homework11/game.cpp
@@ -4,20 +4,38 @@ using namespace std;
 
 int main() {
 
-  bool reached = false;
   stack<int> s;
+  int one, two, three;
   int nums, score, int1;
   cin>>nums;    
   char curr;
   for(int i = 0;i<nums;i++){
     cin>>curr;
     if (curr<='9'&&curr>='0'){
-      int1 = curr - '0';
-    }else if(curr = '+'){
-      while((reached)){
-
-      }
+        curr = curr - '0';
+        s.push(curr);
+    }else if(curr =='+'){
+        one = s.top();
+        s.pop();
+        two = s.top();
+        s.pop();
+        three = one+two;
+        s.push(one);
+        s.push(two);
+        s.push(three);
+    }else if(curr == 'D'){
+        int1 = s.top();
+        int1 = int1*2;
+        s.push(int1);
+    }else if(curr == 'C'){
+        s.pop();
     }
   }
+  while(!(s.empty())){
+    int1 = s.top();
+    s.pop();
+    score+=int1;
 
+  }
+  cout<<score;
 }
