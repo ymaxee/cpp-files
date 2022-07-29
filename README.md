4467cf7d45e4d19d79847c0ebbbfbc307cefe81e
Max <maxlovesairandteslas@gmail.com>
2022-07-28 23:40:45 -0700

    wahh

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 where all of my cpp files
 r located.
 basically a mini cloud
+
+you can ignore this repository if you want but if you want to see my code, \n you can, it's just not really good yet.
+
+### stats
+i use visual studio code and i use this github as a cloud between my devices \n so i don't have to constantly email myself 1600 times.
--- homework11/Greedy Gift Givers/main.cpp
+++ homework11/Greedy Gift Givers/main.cpp
@@ -1,16 +1,58 @@
+/*
+ID: maxlove1
+LANG: C++
+TASK: gift1
+*/
 #include <iostream>
 using namespace std;
 int main() {
     freopen("gift1.in", "r", stdin);
     freopen("gift1.out", "w", stdout);
     int np;
+    string name;
     cin>>np;
-    int peoplecount[np];
-    for(int i = 0;i<np;i++) peoplecount[np] = 0;
+    int amount, number, left, each;
+    int bank[np];
+    for(int i = 0;i<np;i++){bank[i] = 0;}
     string people[np];
     for(int i = 0;i<np;i++){
         cin>>people[np];
     }
-    
+    while(true){
+        int i;
+        cin>>name;
+        for(i = 0;i<np;i++){
+            if(name == people[np]){
+                break;
+            }
+        }
+        cin>>amount;
+        bank[i] = bank[i] - amount;
+        cin>>number;
+        if(number == 0){
+            break;
+        }
+        left = amount%number;
+        amount = amount - left;
+        bank[i] = bank[i] + left;
+        each = amount/number;
+        for(int j = 0;j<number;j++){
+            int k;
+            cin>>name;
+            for(k = 0;k<np;k++){
+            if(name == people[np]){
+                break;
+                }
+            }
+            bank[k] = bank[k] + each;
+
+        }
+    }
+    for(int a = 0;a<np;a++){
+        cout<<people[a]<<" "<<bank[a];
+        if((a+1)<np){
+            cout<<"\n";
+        }
+    }
 
 }
